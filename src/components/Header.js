25df0a4f07bed0c62f2662ba504a23764d43c97e
Norlant1 <jobenbarrera@gmail.com>
2023-01-05 14:30:34 +0800

    add label on incomplete component

--- src/components/Header.js
+++ src/components/Header.js
@@ -171,7 +171,7 @@ const Header = () => {
                  <nav className='user-setting'>
                     <div className='user-setting-container1' onClick={userSetting}><img className='setting' src={require('../images/menuButtons/setting.png')}/><ul className='user-setting-1'>Settings<i style={{fontSize:'0.8em'}}>&#40;In development&#41;</i></ul></div>
                     <div className='user-setting-container2' onClick={SetofNotes}><img className='setofnotes' src={require('../images/menuButtons/setofnotes.png')}/><ul className='user-setting-2'>Set of Notes</ul></div>
-                    <div className='user-setting-container3'><img className='feedback-button' src={require('../images/menuButtons/feedback.png')}/><ul className='user-setting-3'>Feedback</ul></div>
+                    <div className='user-setting-container3'><img className='feedback-button' src={require('../images/menuButtons/feedback.png')}/><ul className='user-setting-3'>Feedback<i style={{fontSize:'0.8em'}}>  &#40;In development&#41;</i></ul></div>
                     <div className='user-setting-container4' onClick={logout}><img className='logout-button' src={require('../images/menuButtons/logout.png')}/><ul className='user-setting-3'>Sign out</ul></div>
                  
                  </nav>
--- src/components/admin/TokenHandlerDocumentation.js
+++ src/components/admin/TokenHandlerDocumentation.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TokenHandlerDocumentation = () => {
   return (
-    <div>TokenHandlerDocumentation</div>
+    <div>TokenHandlerDocumentation<i style={{fontSize:'0.8em'}}>  &#40;In development&#41;</i></div>
   )
 }
 
